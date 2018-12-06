3123c9ddf36ebd7cfc68bdc483062856e8e5ea1e
keerthi <keerthi.a.wiredelta@gmail.com>
2018-12-06 14:57:15 +0100

    goodboy oldboy

---
+++ javascript/javascript1/week1/dogagecalculator.js
@@ -0,0 +1,9 @@
+let dogBirthYear=2015;
+let FutureYear=2022;
+let dogYear=49;
+let shouldShowResultInDogYears;
+if(dogYear == (FutureYear-dogBirthYear)*7)
+    console.log("Your dog will be 49 dog years old in 2022");
+if(dogYear == (FutureYear-dogBirthYear))
+    console.log("Your dog will be 7 human years old in 2022");
+
