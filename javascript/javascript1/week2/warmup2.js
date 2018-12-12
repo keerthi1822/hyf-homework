e89d25c5c7c9535e9c35b3bc9a38260f1480e3f6
keerthi <keerthi.a.wiredelta@gmail.com>
2018-12-12 12:22:23 +0100

    part1 warmup JS 2

---
+++ javascript/javascript1/week2/warmup2.js
@@ -0,0 +1,35 @@
+ //empty object
+ let emptyObject={};
+
+//Function that returns empty object
+function emptyReturnFunction(){
+    
+//returning empty object
+return {} ;
+}
+
+//assigning empty function to time object
+let timeObject= emptyReturnFunction() ;
+
+//Adding time key to timeObject
+timeObject.time=12;
+
+//printing the data of empty and time objects
+console.log(timeObject);
+console.log(emptyObject);
+
+//testing in different ways
+ emptyObject =emptyReturnFunction();
+
+//Addind key value name to empty object
+emptyObject.name="keerthi";
+timeObject.minutes=20;
+timeObject.seconds=40;
+
+//printing the data of empty and time objects
+console.log(timeObject);
+
+//not any more empty Object
+console.log(emptyObject);
+
+
