ed710143d4f32a36599d31cd7c74da516a8c5bc5
keerthi <keerthi.a.wiredelta@gmail.com>
2019-01-10 22:58:37 +0100

    modified homework

--- javascript/javascript1/week3/DOMmanupulation/books.js
+++ javascript/javascript1/week3/DOMmanupulation/books.js
@@ -1,133 +1,156 @@
-let books = ["harrypotter", 
-                "mahabarat", 
-                "ramayan", 
-                "vikrambetal", 
-                "alladin"];
-console.log(books);
+let books = ["harrypotter",
+    "mahabarat",
+    "ramayan",
+    "vikrambetal",
+    "alladin"];
+//console.log(books);
 
 
 let booksInfo = [
-  {
-         id :books[0],
-         title : books[0].toUpperCase(),
-         author :"author1",
-         language: "english"
+    {
+        id: books[0],
+        title: books[0].toUpperCase(),
+        author: "author1",
+        language: "english"
 
     },
-   {
-        id :books[1],
-        title : books[1].toUpperCase(),
-        author :"author2",
+    {
+        id: books[1],
+        title: books[1].toUpperCase(),
+        author: "author2",
         language: "Sanskrit"
     },
-  {
-        id :books[2],
-        title : books[2].toUpperCase(),
-        author :"author3",
+    {
+        id: books[2],
+        title: books[2].toUpperCase(),
+        author: "author3",
         language: "Sanskrit"
-   },
-   {
-        id :books[3],
-        title : books[3].toUpperCase(),
-        author :"author4",
+    },
+    {
+        id: books[3],
+        title: books[3].toUpperCase(),
+        author: "author4",
         language: "Telugu"
-   },
-   {
-        id :books[4],
-        title : books[4].toUpperCase(),
-        author :"author5",
+    },
+    {
+        id: books[4],
+        title: books[4].toUpperCase(),
+        author: "author5",
         language: "Hindi"
     }
 
 ]
 
-let booksImgs = [{
-   img : "Harrypotter.png" ,
-    id : books[0]
-},
-   
-{
-    img : "ramayanimg.jpg" ,
-    id:books[2]
-},
-{
-   img : "vikrambetal.jpg" ,
-    id:books[3]
-},
-{
-    img : "Mahabharat.jpg" ,
-      id:books[1]
-  },
-{
-   img : "alladin.jpg",
-    id:books[4]
+let booksImgs = {
+    harrypotter: "Harrypotter.png",
 
-}]
+    ramayan: "ramayanimg.jpg",
+
+
+    vikrambetal: "vikrambetal.jpg",
+
+
+    mahabarat: "Mahabharat.jpg",
+
+    alladin: "alladin.jpg"
+
+}
 
 //implementing function to add imgs
-function addImages()
-{
-    //selecting parent list
-    let  parentList = document.getElementsByClassName("parentli");
-    console.log( parentList.length);
-    //creating image
-    let imgTag = document.createElement("img");
+// function addImages()
+// {
+//     //selecting parent list
+//     let  parentList = document.getElementsByClassName("parentli");
+//     console.log( parentList.length);
+//     //creating image
 
-    //comparing id's of 2 objects(if returns true add image tag)
-    for (let i=0;i< parentList.length;i++){
-        for (let j=0;j< parentList.length;j++){
-            if(parentList[i].id === booksImgs[j].id ){
-                imgTag.setAttribute("src",booksImgs[j].img)
-                parentList[i].appendChild(imgTag);
-               
-            }
+//     //comparing id's of 2 objects(if returns true add image tag)
+//     for (let i=0;i< parentList.length;i++){
+//         for (let j=0;j< parentList.length;j++){
+//             if(parentList[i].id === booksImgs[j].id ){
+
+//                 let imgTag = document.createElement("img");
+//                 imgTag.setAttribute("src",booksImgs[j].img)
+//                 parentList[i].appendChild(imgTag);
+
+//             }
+
+//         }
+//     }
+// }
+
+function addImages() {
+    console.log(Object.keys(booksImgs));
+    let  parentDiv = document.getElementsByClassName("info");
+    for (i = 0; i < booksInfo.length; i++) {
+        let keyarr = Object.keys(booksImgs);
+        console.log(keyarr);
+        let valArr = Object.values(booksInfo[i]);
+        console.log(valArr);
+
+        const bookId = booksInfo[i].id;
+            console.log(bookId);
+
+           const imgPath = booksImgs[bookId];
+
+        
+                // if (keyarr[i] === valArr[j]) {
+                     let imgTag = document.createElement("img");
+                    imgTag.setAttribute("src", imgPath);
+                    imgTag.style.maxWidth = '300px';
+                    imgTag.style.maxHeight = '400px';
+                    parentDiv[i].appendChild(imgTag);
             
-        }
+
+        
     }
+
 }
 
 
-  
- // implementing function to create list of books and their description
+
+
+
+// implementing function to create list of books and their description
 function generateUlLi() {
 
     let uList = document.createElement("ul");
-   
-     for (let i = 0; i < books.length; i++) {
+
+    for (let i = 0; i < books.length; i++) {
         //console.log("in generateulli");
 
 
-         //creating parent list 
+        //creating parent list 
         let list = document.createElement("li");
         uList.appendChild(list);
         list.setAttribute("id", booksInfo[i].id);
         list.setAttribute("class", 'parentli')
-        
+
 
         //creating H1 element and appending to list
         let heading = document.createElement('h1');
-        heading.innerHTML=booksInfo[i].title;
+        heading.innerHTML = booksInfo[i].title;
         list.appendChild(heading);
 
         //creating div for description of each book
         let divx = document.createElement("div");
-        divx.setAttribute("class","info");
+        divx.setAttribute("class", "info");
 
         //creating unordered list inside div
         let dList = document.createElement("ul");
-        
+
         //description(list) about books 
         let dtitle = document.createElement("li");
-        dtitle.innerHTML =  booksInfo[i].title;
+        dtitle.innerHTML = booksInfo[i].title;
         dList.appendChild(dtitle);
         let dauthor = document.createElement("li");
-        dauthor.innerHTML =  booksInfo[i].author;
+        dauthor.innerHTML = booksInfo[i].author;
         dList.appendChild(dauthor);
         let dlang = document.createElement("li");
-        dlang.innerHTML =  booksInfo[i].language;
+        dlang.innerHTML = booksInfo[i].language;
         dList.appendChild(dlang);
         let dId = document.createElement("li");
-        dId.innerHTML =  booksInfo[i].id;
+        dId.innerHTML = booksInfo[i].id;
         dList.appendChild(dId);
 
         //appendng description list to div
@@ -135,19 +158,18 @@ function generateUlLi() {
 
         //appending div to parent li
         list.appendChild(divx);
-     }
-     //appending parent li to parent ul
-     document.body.appendChild(uList);
-     
+    }
+    //appending parent li to parent ul
+    document.body.appendChild(uList);
+
 
 }
 
 //calling functions
- generateUlLi();
+generateUlLi();
 
- addImages();
+addImages();
 
 
- 
 
- 
+
