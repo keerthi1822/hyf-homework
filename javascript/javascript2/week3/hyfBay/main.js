00e6f083c1467a3274c94188f7dc4f534514641f
keerthi <keerthi1822@gmail.com>
2019-01-31 00:21:20 +0100

    filter by country

--- javascript/javascript2/week3/hyfBay/main.js
+++ javascript/javascript2/week3/hyfBay/main.js
@@ -1,77 +1,72 @@
-console.log('Script loaded');
+console.log("Script loaded");
 
-let sectionProducts = document.querySelector('.products ul');
-
+//selecting element from index.html
+let sectionProducts = document.querySelector(".products ul");
 //console.log(sectionProducts);
 
-let productList= getAvailableProducts();
+//calling function getAvailableProducts
+let productList = getAvailableProducts();
 console.log(productList);
+displayList(productList);
 
-let productDetails = productList.map((product)=>{
-    let list = document.createElement('li');
+//function to display products on screen
+function displayList(list) {
+  let productDetails = list.map(product => {
+    let list = document.createElement("li");
 
-    let name = document.createElement('div');
-    name.setAttribute("class","name");
+    let name = document.createElement("div");
+    name.setAttribute("class", "name");
     name.innerHTML = product.name;
     list.appendChild(name);
 
-    let price = document.createElement('div');
-    price.setAttribute("class","price");
+    let price = document.createElement("div");
+    price.setAttribute("class", "price");
     price.innerHTML = product.price;
     list.appendChild(price);
 
-    let rating = document.createElement('div');
-    rating.setAttribute("class","rating");
+    let rating = document.createElement("div");
+    rating.setAttribute("class", "rating");
     rating.innerHTML = product.rating;
     list.appendChild(rating);
 
-    let shipsTo = document.createElement('div');
-    shipsTo.setAttribute("class","ships-to");
+    let shipsTo = document.createElement("div");
+    shipsTo.setAttribute("class", "shipsTo");
     shipsTo.innerHTML = product.shipsTo;
     list.appendChild(shipsTo);
 
     let button = document.createElement("button");
-    button.setAttribute("data-id","Drone23787")
-    button.innerHTML = "Add to the cart";
+    button.setAttribute("data-id", "Drone23787");
+    button.innerHTML = "+";
     list.appendChild(button);
 
     sectionProducts.appendChild(list);
-    
-});
+  });
+}
+
 //---------------------------------------------------------------
 
 //price analysis
 
-let prices = productList.map((product)=>{
-    return product.price;
+let prices = productList.map(product => {
+  return product.price;
 });
-console.log(sendPricesToServer(prices,()=>{
-    console.log("data received");
-}));
+sendPricesToServer(prices, serverResponse => {
+  console.log(serverResponse);
+});
 
 //---------------------------------------------------------------------
 
-//filter countries
-let countries = productList.filter(filterCountry);
-
-const filterCountry = function (){
-
-}
-
-//filter using search
-let selectTag = document.createElement("select");
-
+//filter using select
+let selectTag = document.querySelector(".country select");
 selectTag.addEventListener("change", () => {
-    
-    let index = selectTag.selectedIndex;
-    let options = document.querySelectorAll("option");
-    
-    return options[index].value
-})
-
-
-
-
-
-
+ 
+  let sCity = selectTag.value;
+  console.log(sCity);
+  console.log(productList);
+  let selectedWithCountry = productList.filter(product =>product.shipsTo.includes (sCity));
+  console.log(selectedWithCountry);
+    displayList(selectedWithCountry);
+  });
+ 
+ 
 
