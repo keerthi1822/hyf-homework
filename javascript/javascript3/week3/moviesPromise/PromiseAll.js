40e169ab24de33dec085be975f8c30dee2b7e163
keerthi <keerthi1822@gmail.com>
2019-02-16 01:14:45 +0100

    promiseAll.js

--- javascript/javascript3/week3/moviesPromise/PromiseAll.js
+++ javascript/javascript3/week3/moviesPromise/PromiseAll.js
@@ -1,56 +1,61 @@
 //Mapping array of fetch object to find owner, fullname, repository urls
-function mapItems(dataItems){
-dataItems.map((dataitem)=>{
+function mapItems(dataItems) {
+  dataItems.map(dataitem => {
     console.log(dataitem.full_name);
     console.log(dataitem.owner.login);
     console.log(dataitem.owner.repos_url);
-    renderList(dataitem.owner.login,dataitem.full_name,dataitem.owner.repos_url);
-  
-});
+    renderList(
+      dataitem.owner.login,
+      dataitem.full_name,
+      dataitem.owner.repos_url
+    );
+  });
 }
 //rendering data to html using DOM
-function renderList(owner,fullname,repourl){
-const uList = document.querySelector('ul');
-    const liElement = document.createElement('li');
-    liElement.innerHTML= owner;
+function renderList(owner, fullname, repourl) {
+  const uList = document.querySelector("ul");
+  const liElement = document.createElement("li");
+  liElement.innerHTML = owner;
 
-    const uListChild = document.createElement('ul');
-    const liElementChild = document.createElement('li');
-    liElementChild.innerHTML= fullname;
-    uListChild.appendChild(liElementChild);
-    
-    const liRepo = document.createElement('li');
-    liRepo.innerHTML = repourl;
-    
-    uListChild.appendChild(liRepo);
+  const uListChild = document.createElement("ul");
+  const liElementChild = document.createElement("li");
+  liElementChild.innerHTML = fullname;
+  uListChild.appendChild(liElementChild);
 
-    liElement.appendChild(uListChild);
-    uList.appendChild(liElement);
+  const liRepo = document.createElement("li");
+  liRepo.innerHTML = repourl;
+
+  uListChild.appendChild(liRepo);
+
+  liElement.appendChild(uListChild);
+  uList.appendChild(liElement);
 }
-//returning 3 promises p1,p2,p3 
-let p1 = fetch('https://api.github.com/search/repositories?q=user:SQasemi')
-    .then(response=> response.json())
-    .then((data)=>{
-        // console.log(data);
-        //  console.log(data.items);
-        //  console.log(data.items[0].owner.repos_url);
-         mapItems(data.items);
-    });
+//returning 3 promises p1,p2,p3
+let p1 = fetch("https://api.github.com/search/repositories?q=user:SQasemi")
+  .then(response => response.json())
+  .then(data => {
+    // console.log(data);
+    //  console.log(data.items);
+    //  console.log(data.items[0].owner.repos_url);
+    mapItems(data.items);
+  });
 
-let p2 = fetch('https://api.github.com/search/repositories?q=user:kseniiazar ')
-.then(response=> response.json())
-.then((data)=>{
+let p2 = fetch("https://api.github.com/search/repositories?q=user:kseniiazar ")
+  .then(response => response.json())
+  .then(data => {
     // console.log(data);
     // console.log(data.items);
     mapItems(data.items);
-});
+  });
 
-let p3 = fetch('https://api.github.com/search/repositories?q=user:humayunadilshahzad')
-.then(response=> response.json())
-.then((data)=>{
+let p3 = fetch(
+  "https://api.github.com/search/repositories?q=user:humayunadilshahzad"
+)
+  .then(response => response.json())
+  .then(data => {
     // console.log(data);
     // console.log(data.items);
-   mapItems(data.items);
-});
+    mapItems(data.items);
+  });
 //using Promise All
-Promise.all([p1,p2,p3]);
+Promise.all([p1, p2, p3]);
