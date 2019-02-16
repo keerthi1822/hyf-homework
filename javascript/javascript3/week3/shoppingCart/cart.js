3468c41c6b1aeed0e69188525b99e8189399a1c7
keerthi <keerthi1822@gmail.com>
2019-02-16 01:08:49 +0100

    promise all done

--- javascript/javascript3/week3/shoppingCart/cart.js
+++ javascript/javascript3/week3/shoppingCart/cart.js
@@ -14,78 +14,83 @@ const availableProducts = [
 ];
 
 class Product {
-    constructor(name, price) {
-        this.name = name;
-        this.price = price;
-    }
-}
-function productsInStockDisplay(){
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+  productsInStockDisplay(){
     //selecting UL tag from HTML
-    const uList = document.querySelector('.displayProducts > ul');
-    //Mapping each product 
-    const instancesOfAvailableProducts = availableProducts.map((product)=>{
-        //create li tag in ul
-        const displayProducts = document.createElement('li');
-        //create attribute class to LI element
-        displayProducts.className="display";
-        //create div element for product
-        const productDivElement = document.createElement('div');
-        //render Product name
-        productDivElement.innerHTML=product;
-        //append product to list
-        displayProducts.appendChild(productDivElement);
-        //create div element for price
-        const priceDivElement = document.createElement('div');
-        //render Price 
-        priceDivElement.innerHTML = Math.floor(Math.random()*10000);
-        //append price div to li element
-        displayProducts.appendChild(priceDivElement);
-        //create button "ADD TO CART"
-        const btn = document.createElement('button');
-        //Name Button
-        btn.innerHTML="Add to cart";
-        //Append button to ul element
-        displayProducts.appendChild(btn);
-        //append Li element to UL element
-        uList.appendChild(displayProducts);
-        return new Product(productDivElement.innerHTML, priceDivElement.innerHTML);
-    })
-    console.log(instancesOfAvailableProducts);
+  const uList = document.querySelector(".displayProducts > ul");
+    //create li tag in ul
+    const displayProducts = document.createElement("li");
+    //create attribute class to LI element
+    displayProducts.className = "display";
+    //create div element for product
+    const productDivElement = document.createElement("div");
+    //render Product name
+    productDivElement.innerHTML = this.name;
+    //append product to list
+    displayProducts.appendChild(productDivElement);
+    //create div element for price
+    const priceDivElement = document.createElement("div");
+    //render Price
+    priceDivElement.innerHTML = this.price;
+    //append price div to li element
+    displayProducts.appendChild(priceDivElement);
+    //create button "ADD TO CART"
+    const btn = document.createElement("button");
+    //Name Button
+    btn.innerHTML = "Add to cart";
+    //Append button to ul element
+    displayProducts.appendChild(btn);
+    //append Li element to UL element
+    uList.appendChild(displayProducts);
+      
+  }
+
 }
 
-productsInStockDisplay();
+
+  
+  for(let elem of document.querySelectorAll('.display button')) {
+  elem.addEventListener("click", event => {
+    shoppingCart.addProduct("elem");
+  });
+}
 
 
 class ShoppingCart {
-    constructor(products) {
-        this.products = products;
-    }
-    
-    addProduct(product) {
-        // Implement functionality here
+  constructor(products) {
+    this.products = products;
+  }
 
-    }
+  addProduct(product) {
+    // Implement functionality here
+    this.products.push(product);
+    console.log(product);
+  }
 
-    removeProduct(product) {
-        // Implement functionality here
-    }
+  removeProduct(product) {
+    // Implement functionality here
+  }
 
-    getTotal() {
-        // Implement functionality here
-    }
+  getTotal() {
+    // Implement functionality here
+  }
 
-    renderProducts() {
-        // Implement functionality here
-    }
+  renderProducts() {
+    // Implement functionality here
 
-    getUser() {
-        // Implement functionality here
-    }
+  }
+
+  getUser() {
+    // Implement functionality here
+  }
 }
-
-const flatscreen = new Product('flat-screen', 5000);
-const shoppingCart = new ShoppingCart([flatscreen]);
-
-document.querySelector('.display button').addEventListener('click',()=>{
-    shoppingCart.addProduct()
+availableProducts.map((product)=>{
+    let products = new Product(product,Math.floor(Math.random()*10000));
+    products.productsInStockDisplay();
 })
+
+const shoppingCart = new ShoppingCart([]);
+
