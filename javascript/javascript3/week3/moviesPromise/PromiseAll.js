80dba60fbb9588427a1e495f8162a9bfe4e3e21f
keerthi <keerthi1822@gmail.com>
2019-03-06 11:59:09 +0100

    promise all homework done

--- javascript/javascript3/week3/moviesPromise/PromiseAll.js
+++ javascript/javascript3/week3/moviesPromise/PromiseAll.js
@@ -1,23 +1,4 @@
-//rendering data to html using DOM
-function renderList(owner, fullname, repourl) {
-  const uList = document.querySelector("ul");
 
-  const liElement = document.createElement("li");
-  liElement.innerHTML = owner;
-
-  const uListChild = document.createElement("ul");
-  const liElementChild = document.createElement("li");
-  liElementChild.innerHTML = fullname;
-  uListChild.appendChild(liElementChild);
-
-  const liRepo = document.createElement("li");
-  liRepo.innerHTML = repourl;
-  uListChild.appendChild(liRepo);
-
-  liElement.appendChild(uListChild);
-  uList.appendChild(liElement);
-}
-//returning 3 promises p1,p2,p3
 const repositoriesUrls = [
   "https://api.github.com/search/repositories?q=user:SQasemi",
   "https://api.github.com/search/repositories?q=user:kseniiazar ",
@@ -26,17 +7,44 @@ const repositoriesUrls = [
 console.log(repositoriesUrls);
 
 const promises = repositoriesUrls.map(repository => fetch(repository));
-console.log(promises);
+//console.log(promises);
 
 Promise.all(promises)
   .then(responses => {
     const promisesJsons = responses.map(response => response.json());
-    console.log(promisesJsons);
+    //console.log(promisesJsons);
     return Promise.all(promisesJsons);
   })
-  .then(repositories=>{
+  .then(repositories => {
     console.log(repositories);
-  })
+    repositories.forEach(repository =>{
+console.log(repository)
+const uList = document.querySelector("ul");
+
+const liElement = document.createElement("li");
+liElement.innerHTML = repository.items[0].owner.login;
+repository.items.forEach(item=>{
+  const uListChild = document.createElement("ul");
+  const liElementChild = document.createElement("li");
+  liElementChild.innerHTML = item.full_name +' -- '+item.git_url;
+  uListChild.appendChild(liElementChild);
+
+  liElement.appendChild(uListChild);
+  //console.log(item.full_name);
+  //console.log(item.git_url);
+})
+uList.appendChild(liElement);
+    });
+
+
+    
+/*     const allReopositoriesAsOneArray = eachRepoItems.flat();
+    console.log(allReopositoriesAsOneArray);
+    allReopositoriesAsOneArray.forEach(eachRepo => {
+      renderList( eachRepo.owner.login, eachRepo.full_name, eachRepo.url)
+      
+    }); */
+  });
 
 //using Promise All
 /* Promise.all([p1, p2, p3])
