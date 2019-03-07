bea6fe0382f49562afa8ec2a33e1b8817de9bb88
keerthi <keerthi1822@gmail.com>
2019-03-07 10:00:00 +0100

    promise program name changed

--- javascript/javascript3/week3/PromiseAlljs/PromiseAll.js
+++ javascript/javascript3/week3/PromiseAlljs/PromiseAll.js
@@ -1,4 +1,3 @@
-
 const repositoriesUrls = [
   "https://api.github.com/search/repositories?q=user:SQasemi",
   "https://api.github.com/search/repositories?q=user:kseniiazar ",
@@ -17,28 +16,26 @@ Promise.all(promises)
   })
   .then(repositories => {
     console.log(repositories);
-    repositories.forEach(repository =>{
-console.log(repository)
-const uList = document.querySelector("ul");
+    repositories.forEach(repository => {
+      console.log(repository);
+      const uList = document.querySelector("ul");
 
-const liElement = document.createElement("li");
-liElement.innerHTML = repository.items[0].owner.login;
-repository.items.forEach(item=>{
-  const uListChild = document.createElement("ul");
-  const liElementChild = document.createElement("li");
-  liElementChild.innerHTML = item.full_name +' -- '+item.git_url;
-  uListChild.appendChild(liElementChild);
+      const liElement = document.createElement("li");
+      liElement.innerHTML = repository.items[0].owner.login;
+      repository.items.forEach(item => {
+        const uListChild = document.createElement("ul");
+        const liElementChild = document.createElement("li");
+        liElementChild.innerHTML = item.full_name + " -- " + item.git_url;
+        uListChild.appendChild(liElementChild);
 
-  liElement.appendChild(uListChild);
-  //console.log(item.full_name);
-  //console.log(item.git_url);
-})
-uList.appendChild(liElement);
+        liElement.appendChild(uListChild);
+        //console.log(item.full_name);
+        //console.log(item.git_url);
+      });
+      uList.appendChild(liElement);
     });
 
-
-    
-/*     const allReopositoriesAsOneArray = eachRepoItems.flat();
+    /*     const allReopositoriesAsOneArray = eachRepoItems.flat();
     console.log(allReopositoriesAsOneArray);
     allReopositoriesAsOneArray.forEach(eachRepo => {
       renderList( eachRepo.owner.login, eachRepo.full_name, eachRepo.url)
